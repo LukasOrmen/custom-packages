e7796193238c950b572b0328aaaa71c54cc04add
LukasOrmen <lukaso922o@gmail.com>
2022-05-06 20:03:36 +0200

    added easier use, with the help of local paths

--- jsonPLUS.py
+++ jsonPLUS.py
@@ -1,8 +1,12 @@
 import pathlib
 import json
 
-def changeatt(dict_, path):
+def changeatt(dict_, path, easypath=False):
     try:
+        if easypath == True:
+            path = os.getcwd() + path
+        else:
+            pass
         counter = 0
         for key, value in dict_.items():
             with pathlib.Path(path).open("r") as f:
@@ -14,10 +18,16 @@ def changeatt(dict_, path):
             print(str(counter) + "th term completed.")
         return "Completed succesfully."
     except AttributeError:
-        return "AttributeError\nchangejsonatt(dictionary, path)"
+        print("AttributeError\nchangeatt(dictionary, path)")
+    except IsADirectoryError:
+        print("IsADirectoryError\changeatt(dict, path)")
 
-def returnatt(list_, path):
+def returnatt(list_, path, easypath=False):
     try:
+        if easypath == True:
+            path = os.getcwd() + path
+        else:
+            pass
         _list = []
         with pathlib.Path(path).open() as f:
             whole = json.loads(f.read())
@@ -25,15 +35,20 @@ def returnatt(list_, path):
             _list.append(whole[str(item)])
         return _list
     except TypeError:
-        return "TypeError\nreturnatt(list, path)"
+        print("TypeError\nreturnatt(list, path)")
     except KeyError:
-        return "TypeError\nreturnatt(list, path)"
+        print("TypeError\nreturnatt(list, path)")
+    except IsADirectoryError:
+        print("IsADirectoryError\returnatt(dict, path)")
 
-def addatt(dict_, path):
-    with pathlib.Path(path).open() as f:
-        original = json.loads(f.read())
-    for key, value in dict_.items():
-        original[key] = value
-        with pathlib.Path(path).open("w") as f:
-            f.write(json.dumps(original))
-    return "Completed succesfully."
+def addatt(dict_, path, easypath=False):
+    try:
+        with pathlib.Path(path).open() as f:
+            original = json.loads(f.read())
+        for key, value in dict_.items():
+            original[key] = value
+            with pathlib.Path(path).open("w") as f:
+                f.write(json.dumps(original))
+        return "Completed succesfully."
+    except IsADirectoryError:
+        print("IsADirectoryError\naddatt(dict, path)")
